aa522535ccf3279769c8611bd2b6a2fc454dcad2
Volodymyr <117123903+dudychvo@users.noreply.github.com>
2025-11-11 21:36:06 +0000

    init

---
+++ src/App.tsx
@@ -0,0 +1,30 @@
+// import { useState } from 'react';
+import pokemonBall from './assets/pokemon-ball.svg';
+import logo from './assets/logo.png';
+import search from './assets/search.svg';
+
+import './App.scss';
+
+function App() {
+  return (
+    <div className='container'>
+      <img src={pokemonBall} alt='#' className='pokemon-ball p-b-one' />
+      <img src={pokemonBall} alt='#' className='pokemon-ball p-b-two' />
+      <img src={pokemonBall} alt='#' className='pokemon-ball p-b-three' />
+      <main>
+        <header>
+          <img src={logo} alt='#' className='logo' />
+          <nav>
+            <input type='text' placeholder='Search eg, ditto or pikachu...' />
+            <button>
+              <img src={search} alt='#' className='search' />
+            </button>
+          </nav>
+        </header>
+        <div></div>
+      </main>
+    </div>
+  );
+}
+
+export default App;
