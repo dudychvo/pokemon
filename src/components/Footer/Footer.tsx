8526d881148833da11318bd9ae6e714b22eab3e8
Volodymyr <117123903+dudychvo@users.noreply.github.com>
2025-11-20 18:13:39 +0000

    add footer files

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,11 @@
+import './Footer.scss';
+
+import groundImg from '../../assets/ground.png';
+
+export const Footer = () => {
+  return (
+    <>
+      <img className='ground-img' src={groundImg} alt='#' />
+    </>
+  );
+};
