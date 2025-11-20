249bd338b8d3aa65425d29883e67c2c0ac7b60e8
Volodymyr <117123903+dudychvo@users.noreply.github.com>
2025-11-20 16:55:13 +0000

    change sprites types

--- src/types/PokemonInfo.ts
+++ src/types/PokemonInfo.ts
@@ -65,9 +65,11 @@ export interface PokemonInfo {
   };
   sprites: {
     other?: {
-      'official-artwork'?: {
-        front_default: string;
-        front_shiny: string;
+      showdown: {
+        back_default?: string;
+        back_shiny?: string;
+        front_default?: string;
+        front_shiny?: string;
       };
     };
   };
