d5e2dba9911032b9866a5c3b5ee07f1b61ce4e00
Volodymyr <117123903+dudychvo@users.noreply.github.com>
2025-11-22 01:02:52 +0000

    fixed footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,18 +1,32 @@
 import './Footer.scss';
 
 import groundImg from '../../assets/ground.png';
+import rockBtn from '../../assets/rock-btn.png';
+import github from '../../assets/github.svg';
 
-export const Footer = () => {
+interface loadMoreProps {
+  loadMore: () => void;
+}
+
+export const Footer = ({ loadMore }: loadMoreProps) => {
   const count = Math.ceil(window.innerWidth / 192);
-
-  console.log(count);
 
   return (
     <>
       <div className='footer'>
-        {Array.from({ length: 7 }).map((_, i) => (
+        {Array.from({ length: count }).map((_, i) => (
           <img key={i} className='groundImg' src={groundImg} alt='Ground' />
         ))}
+        <div className='more-btn' onClick={loadMore}>
+          <p>Dig down</p>
+          <img src={rockBtn} alt='More pokemons' />
+        </div>
+        <div className='github'>
+          <a href='https://github.com/dudychvo/'>
+            <img src={github} alt='Github' />
+          </a>
+        </div>
+        <p className='footerText'>Created with ❤️ by Volodymyr Dudych</p>
       </div>
     </>
   );
