7ff70284a9b8d4f1a3d1e18902079f8765fc7919
Volodymyr <117123903+dudychvo@users.noreply.github.com>
2025-11-24 00:06:00 +0000

    add card modal

--- src/components/PokemonCard/PokemonCard.tsx
+++ src/components/PokemonCard/PokemonCard.tsx
@@ -13,10 +13,13 @@ import cardBg from '../../assets/card-bg.png';
 
 import './PokemonCard.scss';
 
+type CryType = 'latest' | 'legacy';
+
 export const PokemonCard = ({ url }: PokemonInfoURL) => {
   const [pokemon, setPokemon] = useState<PokemonInfo | null>(null);
   const nameRef = useRef<HTMLHeadingElement>(null);
   const [showFront, setShowFront] = useState(true);
+  const [isOpen, setIsOpen] = useState(false);
 
   useEffect(() => {
     const fetch = async () => {
@@ -45,12 +48,23 @@ export const PokemonCard = ({ url }: PokemonInfoURL) => {
   const typeOne = getTypeName(0)?.toLowerCase();
   const typeTwo = getTypeName(1)?.toLowerCase();
 
+  const getAbilityName = (abilityIndex: number) => {
+    return pokemon?.abilities?.[abilityIndex]?.ability.name.replace(/^./, (c) =>
+      c.toUpperCase()
+    );
+  };
+
+  const getCryName = (cry: CryType) => {
+    return pokemon?.cries?.[cry];
+  };
+
   const handleImageLoad = (e: React.SyntheticEvent<HTMLImageElement>) => {
     const w = e.currentTarget.naturalWidth;
     e.currentTarget.classList.add(w > 87 ? 'pk-img-wide' : 'pk-img-narrow');
   };
 
-  const [isOpen, setIsOpen] = useState(false);
+  const urlOne = getCryName('latest');
+  const urlTwo = getCryName('legacy');
 
   return (
     <>
@@ -77,23 +91,100 @@ export const PokemonCard = ({ url }: PokemonInfoURL) => {
             onClose={() => setIsOpen(false)}
             className='modal'
           >
-            {/* Overlay */}
             <div className='overlay' aria-hidden='true' />
-
-            {/* Centering wrapper */}
             <div className='modal-wrapper'>
               <DialogPanel className='modal-panel'>
-                <DialogTitle>Deactivate account</DialogTitle>
+                <DialogTitle className='pk-m-name'>{pokemonName}</DialogTitle>
                 <Description>
-                  This will permanently deactivate your account
+                  <h2 className='pk-m-num'>{'#' + pokemon?.id}</h2>
                 </Description>
-
-                <p>Are you sure you want to deactivate your account?</p>
-
-                <div className='buttons'>
-                  <button onClick={() => setIsOpen(false)}>Cancel</button>
-                  <button onClick={() => setIsOpen(false)}>Deactivate</button>
+                <p className='pk-m-cry'>Cries:</p>
+                {getCryName('latest') && (
+                  <button
+                    className='pk-m-cry-one'
+                    onClick={() => new Audio(urlOne).play()}
+                  >
+                    ♪
+                  </button>
+                )}
+                {getCryName('legacy') && (
+                  <button
+                    className='pk-m-cry-two'
+                    onClick={() => new Audio(urlTwo).play()}
+                  >
+                    ♫
+                  </button>
+                )}
+                <img
+                  className='pk-m-img'
+                  src={
+                    showFront
+                      ? pokemon?.sprites.other?.showdown?.front_default
+                      : pokemon?.sprites.other?.showdown?.back_default
+                  }
+                  alt=''
+                  onLoad={handleImageLoad}
+                  onClick={() => setShowFront((prev) => !prev)}
+                />
+                <p className='pk-m-type'>Type:</p>
+                {getTypeName(0) && (
+                  <button className={`pk-m-type-one type-${typeOne}`}>
+                    {getTypeName(0)}
+                  </button>
+                )}
+                {getTypeName(1) && (
+                  <button className={`pk-m-type-two type-${typeTwo}`}>
+                    {getTypeName(1)}
+                  </button>
+                )}
+                <p className='pk-m-abilities'>Abilities:</p>
+                {getAbilityName(0) && (
+                  <div className={`pk-m-ability-one`}>{getAbilityName(0)}</div>
+                )}
+                {getAbilityName(1) && (
+                  <div className={`pk-m-ability-two`}>{getAbilityName(1)}</div>
+                )}
+                <p className='pk-m-body-m'>Body-metrics:</p>
+                <div className={'pk-m-base-exp'}>
+                  <span className='base-exp'>Base-exp:</span>
+                  {pokemon?.base_experience}
                 </div>
+                <div className={'pk-m-weight'}>
+                  <span className='Weight'>Weight:</span>
+                  {pokemon?.weight}
+                </div>
+                <div className={'pk-m-height'}>
+                  <span className='height'>Height:</span>
+                  {pokemon?.height}
+                </div>
+                <p className='pk-m-stats'>Stats:</p>
+                <div className={'pk-m-hp'}>
+                  <span>HP:</span>
+                  {pokemon?.stats[0].base_stat}
+                </div>
+                <div className={'pk-m-attack'}>
+                  <span>Attack:</span>
+                  {pokemon?.stats[1].base_stat}
+                </div>
+                <div className={'pk-m-defense'}>
+                  <span>Defense:</span>
+                  {pokemon?.stats[2].base_stat}
+                </div>
+                <div className={'pk-m-special-attack'}>
+                  <span>S-attack:</span>
+                  {pokemon?.stats[3].base_stat}
+                </div>
+                <div className={'pk-m-special-defense'}>
+                  <span>S-defense:</span>
+                  {pokemon?.stats[4].base_stat}
+                </div>
+                <div className={'pk-m-speed'}>
+                  <span>Speed:</span>
+                  {pokemon?.stats[5].base_stat}
+                </div>
+                <button className='m-close' onClick={() => setIsOpen(false)}>
+                  ✗
+                </button>
               </DialogPanel>
             </div>
           </Dialog>
